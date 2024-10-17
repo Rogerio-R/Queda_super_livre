a12b41c30c9dfec7b62a668d2d0a0b9ce601ee76
Rogerio-R <rogerio.rabelo.filho@escola.pr.gov.br>
2024-10-17 11:34:04 -0300

    dada

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let angulo = 0;
+let velocidade = 1; // Define a velocidade inicial
+let intervalo;
 
 function girarQuadrado() {
     angulo += 90; // Aumenta o ângulo em 90 graus
     document.getElementById('quadrado').style.transform = `rotate(${angulo}deg)`;
 }
 
-// Adiciona um evento de clique ao quadrado
-document.getElementById('quadrado').addEventListener('click', girarQuadrado);
+function ativarRotacao() {
+    clearInterval(intervalo); // Limpa qualquer intervalo anterior
+    intervalo = setInterval(girarQuadrado, 1000 / velocidade);
+}
+
+document.getElementById('girarBtn').addEventListener('click', ativarRotacao);
+
+document.getElementById('velocidadeInput').addEventListener('input', function() {
+    velocidade = this.value;
+    document.getElementById('velocidadeLabel').innerText = `Velocidade: ${velocidade}x`;
+    ativarRotacao(); // Atualiza a rotação com a nova velocidade
+});
